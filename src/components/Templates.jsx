9d9cd5e8beb78c50023d87040506a2cc75badf42
Weintaube <john.lena@t-online.de>
2023-11-21 16:00:21 +0100

    added something for template counting

--- src/components/Templates.jsx
+++ src/components/Templates.jsx
@@ -1,5 +1,6 @@
 import  {React, useEffect} from "react";
 import Table from 'react-bootstrap/Table';
+import Pagination from 'react-bootstrap/Pagination';
 
 function Templates(){
 
@@ -9,8 +10,19 @@ function Templates(){
 
     const fetchData = async()=>{
         try{
-            const response = await fetch('https://orkg.org/api/classes/NodeShape/resources/'); //445 Templates in total
-            const response2 = await fetch('http://orkg.org/api/resources/R599005');
+            //const response = await fetch('https://orkg.org/api/classes/NodeShape/resources/'); //445 Templates in total
+            /*const response = await fetch('http://orkg.org/api/resources/R599005', {header:
+            {
+                'Access-Control-Allow-Origin':'*'
+            }
+            });*/
+
+            //const response = await fetch('https://orkg.org/api/resources/R71449');
+            //const response = await fetch('http://orkg.org/api/classes/C1004');
+
+            const response = await fetch('https://orkg.org/api/statements/predicate/sh:targetClass?page=0'); //nodeshape (template) sh:targetClass Classwhichdescribestemplate, 436
+            //const resp = await fetch('http://orkg.org/api/classes/C21029/resources/');
+
             //a template has an ID with R, they have instances where they are used
             //these instances have a class which also describes the template
             //as far as I can see there is no possibility to connect the template ID with the class ID
@@ -18,9 +30,15 @@ function Templates(){
             //or I can't track it
             if(response.ok){ //Anfrage erfolgreich Statuscode 200
                 console.log("Response (OK)",  response)
-                const result = await response.json();
+                const result = await response.json(); //get a paginated content with 436 elements
+                const result2 = result.content[0].object.id;
+                let ids = result.content.map(some => some.object.id);
+                const targets = await fetch('https://orkg.org/api/classes/{id}/resources/'); //length?
+                console.log(ids);
+                //const result2 = await resp.json();
                 console.log("Templates");
                 console.log(result);
+                console.log(result2);
             }
 
 

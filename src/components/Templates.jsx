fe7d723c93590db0a70a41d1305ec4b5f012cbd6
Lena Weintaube <john.lena@t-online.de>
2023-11-22 13:26:36 +0100

    working basic table with 10 items

--- src/components/Templates.jsx
+++ src/components/Templates.jsx
@@ -1,52 +1,62 @@
-import  {React, useEffect} from "react";
+import  {React, useEffect, useState} from "react";
 import Table from 'react-bootstrap/Table';
 import Pagination from 'react-bootstrap/Pagination';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Templates(){
 
+    const [results, setResults] = useState([]);
+    const [pageNo, setCurrentPage] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
+
     useEffect(() => {            
         fetchData();
-    }, []);
+    }, [pageNo]);
 
     const fetchData = async()=>{
         try{
-            //const response = await fetch('https://orkg.org/api/classes/NodeShape/resources/'); //445 Templates in total
-            /*const response = await fetch('http://orkg.org/api/resources/R599005', {header:
-            {
-                'Access-Control-Allow-Origin':'*'
-            }
-            });*/
+            const response = await fetch(`https://orkg.org/api/statements/predicate/sh:targetClass?page=${pageNo}&size=10`); //nodeshape (template) sh:targetClass Classwhichdescribestemplate, 436
 
-            //const response = await fetch('https://orkg.org/api/resources/R71449');
-            //const response = await fetch('http://orkg.org/api/classes/C1004');
-
-            const response = await fetch('https://orkg.org/api/statements/predicate/sh:targetClass?page=0'); //nodeshape (template) sh:targetClass Classwhichdescribestemplate, 436
-            //const resp = await fetch('http://orkg.org/api/classes/C21029/resources/');
-
-            //a template has an ID with R, they have instances where they are used
-            //these instances have a class which also describes the template
-            //as far as I can see there is no possibility to connect the template ID with the class ID
-            //where it is used, especially if the template is not used, then there is no class I guess
-            //or I can't track it
-            if(response.ok){ //Anfrage erfolgreich Statuscode 200
+            if(response.ok){ 
                 console.log("Response (OK)",  response)
                 const result = await response.json(); //get a paginated content with 436 elements
-                const result2 = result.content[0].object.id;
-                let ids = result.content.map(some => some.object.id);
-                const targets = await fetch('https://orkg.org/api/classes/{id}/resources/'); //length?
-                console.log(ids);
-                //const result2 = await resp.json();
-                console.log("Templates");
-                console.log(result);
-                console.log(result2);
-            }
+                console.log("first result", result);
+                let templateRows = result.content.map(some => ({ label: some.subject.label, created_by: some.subject.created_by, id: some.object.id}));
+                //todo add uri to make clickable
+                console.log("Template rows", templateRows);
 
+                let updatedResults = results.slice();
+                for (let item of templateRows) {
+                    try {
+                        let instances = await fetch(`https://orkg.org/api/classes/${item.id}/resources/`);
+                        
+                        if (instances.ok) {
+                            let resourcesInstances = await instances.json();
+                            let numberinstances = resourcesInstances.totalElements;
+                            updatedResults.push({label: item.label, created_by: item.created_by, numberOfInstances: numberinstances});
+
+                        } else {
+                            console.error(`Fehler beim Abrufen der Ressource für ID ${item.id}: ${response.status}`);
+                        }
+                    } catch (error) {
+                        console.error(`Fehler beim Abrufen der Ressource für ID ${item.id}: ${error}`);
+                    }
+                }
+                updatedResults.sort((a, b) => a.numberOfInstances - b.numberOfInstances); //aufsteigend nach number of instances sortieren
+                //für absteigend b-a rechnen, todo mit UI verstellbar machen
+                setResults(updatedResults);
+                console.log("updated Results", updatedResults);
+            }
 
         }catch(error){
             console.log(error);
         }
     }
 
+    const handlePageIncrement = () => {
+        setCurrentPage(prevPage => prevPage + 1);
+    };
 
     return(
         <>
@@ -59,9 +69,24 @@ function Templates(){
         </tr>
         </thead>
         <tbody>
-
+            {results.map((item, index) => (
+            <tr key={index}>
+                <td>{item.label}</td>
+                <td>{item.created_by}</td>
+                <td>{item.numberOfInstances}</td>
+            </tr>
+        ))}
         </tbody>
         </Table>
+        <Row>
+            <Col>Currently page x from y</Col>
+            <Col>
+                <Pagination>
+                    <Pagination.Prev onClick={handlePageIncrement}/>
+                    <Pagination.Next />
+                </Pagination>     
+            </Col>
+        </Row>
         </>
     );
 }

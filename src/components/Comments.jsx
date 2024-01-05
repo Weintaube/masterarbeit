463d09b4b426fe2f69dccd9f481641774a6c9bfd
Lena Weintaube <john.lena@t-online.de>
2024-01-05 10:50:24 +0100

    added comparison cells

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -144,6 +144,14 @@ function Comments(){
                             </Form.Select>
                         </Form.Group>
 
+                        <Form.Group className="mb-3" controlId="formID">
+                            <Form.Label>ID</Form.Label>
+                            <Form.Control name="formID" type="text" placeholder="Enter ID of resource" required/>
+                            <Form.Control.Feedback type="invalid">
+                                Please enter an ID.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+
                         <Form.Group className="mb-3" controlId="formTypeComment">
                             <Form.Label>Type of comment</Form.Label>
                             <Form.Select name="formTypeComment">
@@ -151,14 +159,6 @@ function Comments(){
                                     <option key={index}>{type}</option>
                                 ))}
                             </Form.Select>
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formID">
-                            <Form.Label>ID</Form.Label>
-                            <Form.Control name="formID" type="text" placeholder="Enter ID of resource" required/>
-                            <Form.Control.Feedback type="invalid">
-                                Please enter an ID.
-                            </Form.Control.Feedback>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formComment">
